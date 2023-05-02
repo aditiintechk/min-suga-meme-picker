51d554233e07e9d634a224b44beb7b3c26e98561
Aditi Deshpande <aditideshpande2908@gmail.com>
2023-05-02 20:35:48 +0530

    Add initial files

---
+++ data.js
@@ -0,0 +1,222 @@
+export const yoongiData = [
+    {
+        emotionTags: ["heart"],
+        image: "bigheart.gif",
+        alt: "yoongi making a big heart",
+    },
+    {
+        emotionTags: ["heart"],
+        image: "bigheart1.gif",
+        alt: "yoongi making a big heart",
+    },
+    {
+        emotionTags: ["confused"],
+        image: "confused.gif",
+        alt: "yoongi looking confused",
+    },
+    {
+        emotionTags: ["confused", "phased out"],
+        image: "confused1.gif",
+        alt: "yoongi looking confused",
+    },    
+    {
+        emotionTags: ["confused", "iconic :] face"],
+        image: "confusedandiconic.gif",
+        alt: "yoongi looking confused and making :] face",
+    },
+    {
+        emotionTags: ["dancing", "happy"],
+        image: "dancing_happy.gif",
+        alt: "yoongi dancing happily",
+    },
+    {
+        emotionTags: ["dramatic"],
+        image: "dramatic.gif",
+        alt: "yoongi being a dramatic ass",
+    },
+    {
+        emotionTags: ["dramatic"],
+        image: "dramatic1.gif",
+        alt: "yoongi being a dramatic ass",
+    },
+    {
+        emotionTags: ["disgusted"],
+        image: "disgusted.gif",
+        alt: "yoongi making disgusted face",
+    },
+    {
+        emotionTags: ["disgusted", "confused"],
+        image: "disgustedandconfused.gif",
+        alt: "yoongi making disgusted face",
+    },
+    {
+        emotionTags: ["shy"],
+        image: "embarrased.gif",
+        alt: "yoongi being embarrased",
+    },
+    {
+        emotionTags: ["shy","cute"],
+        image: "embarrased1.gif",
+        alt: "yoongi being embarrased",
+    },
+    {
+        emotionTags: ["shy","cute"],
+        image: "embarrased2.gif",
+        alt: "yoongi being embarrased",
+    },
+    {
+        emotionTags: ["shy","happy"],
+        image: "embarrasedandhappy.gif",
+        alt: "yoongi being embarrased and happy",
+    },
+    {
+        emotionTags: ["finger heart"],
+        image: "fingerhearts.gif",
+        alt: "yoongi making finger hearts",
+    },
+    {
+        emotionTags: ["finger heart"],
+        image: "fingerhearts1.gif",
+        alt: "yoongi making finger hearts",
+    },
+    {
+        emotionTags: ["frustrated"],
+        image: "frustrated.gif",
+        alt: "yoongi making finger hearts",
+    },
+    {
+        emotionTags: ["gummy smile"],
+        image: "gummysmile.gif",
+        alt: "yoongi and his cute gummy smile hehe",
+    },
+    {
+        emotionTags: ["gummy smile"],
+        image: "gummysmile1.gif",
+        alt: "yoongi and his cute gummy smile hehe",
+    },
+    {
+        emotionTags: ["happy"],
+        image: "happy.gif",
+        alt: "yoongi happy",
+    },
+    {
+        emotionTags: ["happy", "excited"],
+        image: "happyandexcited.gif",
+        alt: "yoongi happy",
+    },
+    {
+        emotionTags: ["happy", "excited"],
+        image: "happyandexcited1.gif",
+        alt: "yoongi happy",
+    },
+    {
+        emotionTags: ["iconic :] face"],
+        image: "iconic.gif",
+        alt: "yoongis very own :] face",
+    },
+    {
+        emotionTags: ["iconic :] face"],
+        image: "iconic1.gif",
+        alt: "yoongis very own :] face",
+    },
+    {
+        emotionTags: ["iconic :] face"],
+        image: "iconic2.gif",
+        alt: "yoongis very own :] face",
+    },
+    {
+        emotionTags: ["iconic :] face"],
+        image: "iconic3.gif",
+        alt: "yoongis very own :] face",
+    },
+    {
+        emotionTags: ["iconic :] face", "confused"],
+        image: "iconicandconfused.gif",
+        alt: "yoongis very own :] face",
+    },
+    {
+        emotionTags: ["laughing"],
+        image: "laughing.gif",
+        alt: "yoongi laughing",
+    },
+    {
+        emotionTags: ["laughing", "shy"],
+        image: "laughingandshy.gif",
+        alt: "yoongi laughing",
+    },
+    {
+        emotionTags: ["laughing"],
+        image: "laughinghard.gif",
+        alt: "yoongi laughing",
+    },
+    {
+        emotionTags: ["phased out"],
+        image: "phasedout.gif",
+        alt: "yoongi phased out",
+    },
+    {
+        emotionTags: ["phased out"],
+        image: "phasedout1.gif",
+        alt: "yoongi phased out",
+    },
+    {
+        emotionTags: ["relaxed"],
+        image: "relaxed.gif",
+        alt: "yoongi relaxed in channel shibuya!",
+    },
+    {
+        emotionTags: ["phased out", "relaxed"],
+        image: "relaxedandphasedout.gif",
+        alt: "yoongi relaxed and calm",
+    },
+    {
+        emotionTags: ["sad"],
+        image: "sad.gif",
+        alt: "yoongi sad :(",
+    },
+    {
+        emotionTags: ["savvy"],
+        image: "savvy.gif",
+        alt: "my savvy min cat suga",
+    },
+    {
+        emotionTags: ["savvy"],
+        image: "savvy1.gif",
+        alt: "my savvy min cat suga",
+    },
+    {
+        emotionTags: ["savvy"],
+        image: "savvy2.gif",
+        alt: "my savvy min cat suga",
+    },
+    {
+        emotionTags: ["savvy", "laughing"],
+        image: "savvyandlaughing.gif",
+        alt: "my savvy min cat suga",
+    },
+    {
+        emotionTags: ["scary"],
+        image: "scary.gif",
+        alt: "scary min cat suga",
+    },
+    {
+        emotionTags: ["sleepy"],
+        image: "sleepy.gif",
+        alt: "sleepy min cat suga",
+    },
+    {
+        emotionTags: ["sleepy"],
+        image: "sleepy1.gif",
+        alt: "sleepy min cat suga",
+    },
+    {
+        emotionTags: ["sleepy"],
+        image: "sleepy2.gif",
+        alt: "sleepy min cat suga",
+    },
+    {
+        emotionTags: ["sleepy"],
+        image: "sleepingonacouch.gif",
+        alt: "sleepy min cat suga",
+    },
+]
---
+++ index.js
@@ -0,0 +1,14 @@
+import { yoongiData } from "./data"
+
+function getEmotionsArray(cats){
+    const emotionsArray = []
+    for (let i = 0; i < cats.length; i++){
+        for (let j=0; j < cats[i].emotionTags.length; j++){
+                emotionsArray.push(cats[i].emotionTags[j])
+        }
+    }
+console.log(emotionsArray)
+}
+
+// getEmotionsArray(catsData)
+
